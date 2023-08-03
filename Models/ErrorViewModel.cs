e5c7e993f6c5af141f1613ad62f846fe6af924f2
Andy St Fort <andy.st.fort@niceincontact.com>
2023-08-03 12:44:53 -0400

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dvd_rental_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
