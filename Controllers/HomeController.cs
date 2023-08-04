62c944d5ba52739acdb939ce4598483aad2f853f
Andy St Fort <andy.st.fort@niceincontact.com>
2023-08-04 16:05:26 -0400

    Methods to be instrumented  On branch local  Your branch is up to date with 'origin/local'.
     Changes to be committed:
    	modified:   Controllers/HomeController.cs
    	modified:   Views/Home/Index.cshtml
    	new file:   Views/Home/MovieDetails.cshtml
    	modified:   Views/Home/Movies.cshtml
    	new file:   Views/Home/MoviesByGenre.cshtml
    	modified:   Views/Shared/_Layout.cshtml
    	modified:   bin/Debug/net7.0/dvd_rental_app.dll
    	modified:   bin/Debug/net7.0/dvd_rental_app.pdb
    	modified:   movies_db.sql
    	modified:   obj/Debug/net7.0/dvd_rental_app.GeneratedMSBuildEditorConfig.editorconfig
    	modified:   obj/Debug/net7.0/dvd_rental_app.csproj.FileListAbsolute.txt
    	modified:   obj/Debug/net7.0/dvd_rental_app.dll
    	modified:   obj/Debug/net7.0/dvd_rental_app.pdb
    	modified:   obj/Debug/net7.0/ref/dvd_rental_app.dll
    	modified:   obj/Debug/net7.0/refint/dvd_rental_app.dll

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,8 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using dvd_rental_app.Models;
 using dvd_rental_app.Data;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dvd_rental_app.Controllers;
 
@@ -12,25 +12,76 @@ public class HomeController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly ApplicationDbContext _db;
 
+
     public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
     {
         _logger = logger;
         _db = db;
     }
 
+
     public DbSet<Movie> Movie { get; set; } = default!;
 
+
+    [Route("/")]
+    [Route("Home/")]
+    [Route("Home/Index")]
     public IActionResult Index()
     {
+        _logger.LogInformation("Home page retrieved and loading...");
         return View();
     }
 
-    public IActionResult Movies()
-    {
-        var MovieList = _db.Movie.ToList();
 
+    [Route("Home/Movies")]
+    public async Task<IActionResult> Movies()
+    {
+        if (_db.Movie == null)
+        {
+            _logger.LogError("Database data for movies not found. Pelase check and try again!");
+            return NotFound();
+        }
+
+        var MovieList = await _db.Movie.ToListAsync();
+
+        _logger.LogInformation("Movie list retrieved successfully.");
         return View(MovieList);
     }
+
+
+    [Authorize]
+    [Route("Home/Movie/Details/{id}")]
+    public async Task<IActionResult> MovieDetails(int id)
+    {
+        var movie = await _db.Movie.Where(m => m.MovieId == id).FirstOrDefaultAsync();
+
+        if (movie == null)
+        {
+            _logger.LogError("Could not find a movie with the specified ID. Pelase check and try again!");
+        }
+
+        _logger.LogInformation("Movie list retrieved successfully.");
+        return View(movie);
+    }
+
+
+    [Authorize]
+    [Route("Home/MoviesByGenre/{genre}")]
+    public async Task<IActionResult> MoviesByGenre(string genre)
+    {
+        var movies = await _db.Movie.Where(m => m.Genre == genre).ToListAsync();
+
+        if (movies == null)
+        {
+            _logger.LogError("Could not find any movie with the specified genre. Pelase check and try again!");
+        }
+
+        ViewBag.Genre = genre;
+
+        _logger.LogInformation("Movie(s) with specified genre retrieved successfully.");
+        return View(movies);
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
